65a0f5b1354e3ab7f48693eed502bf00870a6a4b
unkuseni <amudalatadeoye@gmail.com>
2025-03-17 00:33:49 +0100

    Add error handling and not found page for invoice editing; enhance create and update invoice functions with try-catch

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,11 +25,15 @@ export async function createInvoice(formData: FormData) {
 	});
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
-
-	await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+	try {
+		await sql`
+	INSERT INTO invoices (customer_id, amount, status, date)
+	VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+	} catch (error) {
+		console.error("Error creating invoice:", error);
+		throw error;
+	}
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
 }
@@ -44,18 +48,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 	});
 
 	const amountInCents = amount * 100;
-
-	await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
+	try {
+		await sql`
+	UPDATE invoices
+	SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+	WHERE id = ${id}
   `;
+	} catch (error) {
+		console.error("Error updating invoice:", error);
+		throw error;
+	}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-	await sql`DELETE FROM invoices WHERE id = ${id}`;
+	try {
+		await sql`DELETE FROM invoices WHERE id = ${id}`;
+	} catch (error) {
+		console.error("Error deleting invoice:", error);
+		throw error;
+	}
 	revalidatePath("/dashboard/invoices");
 }
